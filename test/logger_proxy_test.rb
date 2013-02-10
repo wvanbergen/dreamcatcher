ca90270a4d96dc6345deb2a412102212b2b0913c
Willem van Bergen <willem@vanbergen.org>
2013-02-10 14:56:01 -0500

    Add a test for correct behaviour when not using a logger object.

--- test/logger_proxy_test.rb
+++ test/logger_proxy_test.rb
@@ -5,13 +5,20 @@ class LoggerProxyTest < Test::Unit::TestCase
 
   def test_logger_capture
     logger = Logger.new("/dev/null")
-    proxy = Dreamcatcher::LoggerProxy.new(logger)
-    proxy.capture do
+    entries = Dreamcatcher::LoggerProxy.capture(logger) do
       logger.info "hello world"
     end
     logger.info "goodbye world"
 
-    assert_equal proxy.log_entries.size, 1
-    assert_equal proxy.log_entries.first.message, "hello world"
+    assert_equal entries.size, 1
+    assert_equal entries.first.message, "hello world"
+  end
+
+  def test_capture_without_logger
+    logger = Logger.new("/dev/null")
+    entries = Dreamcatcher::LoggerProxy.capture(nil) do
+      logger.info "hello world"
+    end
+    assert_equal entries.size, 0
   end
 end
